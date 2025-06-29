85d731faabc12881fe948ba623972e9669ffe05e
gaetanZipp <lagrange.bagze@facsciences-uy1.cm>
2025-06-29 12:56:03 +0100

    first commit

---
+++ src/main/java/com/example/systemProduct/SystemProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.systemProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/systemProduct/SystemProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.systemProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
